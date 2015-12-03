5757f4c8ab5e5fe9bf3170ab7c4e9cc11a37160e
Giuliano <delapenna.g@gmail.com>
2015-12-03 15:52:43 -0300

    Agregadas clases de dominio Spring Security

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -200,3 +200,22 @@ environments {
     }
 }
 
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.colaboratorio.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.colaboratorio.UserRole'
+grails.plugin.springsecurity.authority.className = 'com.colaboratorio.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']]
+]
+
